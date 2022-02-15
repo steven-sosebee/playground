e17cd4f883af4fc5508ca18c4871f6cc37f11c25
Steven Sosebee <swsosebee@gmail.com>
2022-02-14 19:23:28 -0500

    mobile design, engine for heroku

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -21,38 +21,39 @@ export const Sidebar = () => {
   };
 
   return (
-    // <div className="sidebar-wrapper">
-    <div className={`${theme} ${expanded}`}>
-      <div id="menu-icon" onClick={handleExpandMenu}>
-        <i className="fas fa-align-justify"></i>
-      </div>
-      <button onClick={handleToggleTheme} className="theme-toggle">
-        {theme}
-      </button>
-      <div>
-        <span>{value ? "dark" : "light"}</span>
-      </div>
-      <ul className="sidebar-menu">
-        <li className="sidebar-menu-item">
-          <i className="fas fa-home"></i>{" "}
-          <span className="sidebar-text"> Home</span>
-        </li>
-        <li className="sidebar-menu-item">
-          <i className="fas fa-layer-group"></i>
-          <span className="sidebar-text"> Projects</span>
-        </li>
-        <li className="sidebar-menu-item">
-          <i className="fas fa-user-alt"></i>{" "}
-          <span className="sidebar-text"> Contact</span>
-        </li>
-      </ul>
-      <div className="sidebar-menu-item toggle-item">
-        <span className="sidebar-text ">Dark Mode: </span>
-        {/* <Toggle isOn={value} handleToggle={() => setValue(!value)}></Toggle> */}
-        <Toggle isOn={value} handleToggle={() => handleToggleTheme()}></Toggle>
+    <div className="sidebar-wrapper">
+      <div className={`${theme} ${expanded}`}>
+        <div id="menu-icon" onClick={handleExpandMenu}>
+          <i className="fas fa-align-justify"></i>
+        </div>
+        {/* <button onClick={handleToggleTheme} className="theme-toggle">
+          {theme}
+        </button> */}
+        <div>{/* <span>{value ? "dark" : "light"}</span> */}</div>
+        <ul className="sidebar-menu">
+          <li className="sidebar-menu-item">
+            <i className="fas fa-home"></i>{" "}
+            <span className="sidebar-text"> Home</span>
+          </li>
+          <li className="sidebar-menu-item">
+            <i className="fas fa-layer-group"></i>
+            <span className="sidebar-text"> Projects</span>
+          </li>
+          <li className="sidebar-menu-item">
+            <i className="fas fa-user-alt"></i>{" "}
+            <span className="sidebar-text"> Contact</span>
+          </li>
+        </ul>
+        <div className="sidebar-menu-item toggle-item">
+          <span className="sidebar-text ">Dark Mode: </span>
+          {/* <Toggle isOn={value} handleToggle={() => setValue(!value)}></Toggle> */}
+          <Toggle
+            isOn={value}
+            handleToggle={() => handleToggleTheme()}
+          ></Toggle>
+        </div>
       </div>
     </div>
-    // </div>
   );
 };
 
