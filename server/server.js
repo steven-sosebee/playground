eaebc767f1cab9a9c9aad1bb76a6e7b405619309
Steven Sosebee <swsosebee@gmail.com>
2022-04-26 10:14:53 -0400

    favicon issue

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "prod") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
